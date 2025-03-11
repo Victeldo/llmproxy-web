883f011984003f4b7a3c663459e6731c2b142bbe
Prithvi <44360797+Victeldo@users.noreply.github.com>
2025-03-11 11:36:21 -0400

    increase article count significantly

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def news_fetching_agent(query_term):
         "q": query_term,
         "from": from_date,
         "sortBy": "popularity",
-        "pageSize": 5,
+        "pageSize": 100,
         "apiKey": news_key
     }
     articles = []
